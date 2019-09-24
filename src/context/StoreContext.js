2d6bcb5c465bb4f4ee5a1b5ec964e9c07e2af7a2
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-09-24 16:06:02 -0500

    updating for cart issue

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -1,16 +1,29 @@
 import React, { createContext, useState } from 'react'
+import Client from 'shopify-buy'
+
+const client = Client.buildClient({
+    domain: "web-wabi-sabi.myshopify.com",
+    storefrontAccessToken: "50c676159b0e37611699ecd324eae2c2"
+})
 
 const defaultValues = {
         isCartOpen: false,
         cart: [],
-        addProductToCart: () => { console.log('added')
-    },
+        addProductToCart: () => {},
+    client,
 }
 export const StoreContext = createContext(defaultValues)
 
 export const StoreProvider = ({children}) => {
+    const addProductToCart = () => {
+        console.log("!");
+    }
+
     return(
-        <StoreContext.Provider value={defaultValues}>
+        <StoreContext.Provider value={{
+            ...defaultValues,
+            addProductToCart
+            }}>
             {children}
         </StoreContext.Provider>
     )
